abc0aa7798bd640b1d30e0d672d2707e945eac68
Sreenath Joseph <sreenath.joseph@gmail.com>
2026-01-19 13:44:50 +0530

    feat: expand Bengaluru tech park registry including DivyaSree Greens

--- src/constants/techParks.ts
+++ src/constants/techParks.ts
@@ -71,6 +71,76 @@ export const TECH_PARKS: Location[] = [
 		lat: 12.9912,
 		lng: 77.659,
 	},
+	{
+		name: "Embassy Tech Village (ETV), Bellandur",
+		lat: 12.9248,
+		lng: 77.6826,
+	},
+	{
+		name: "IBC Knowledge Park, Bannerghatta Road",
+		lat: 12.936,
+		lng: 77.5996,
+	},
+	{
+		name: "DivyaSree Technopolis, Yemlur",
+		lat: 12.9431,
+		lng: 77.6811,
+	},
+	{
+		name: "Kalyani Tech Park, Brookefield",
+		lat: 12.9723,
+		lng: 77.7145,
+	},
+	{
+		name: "Global Technology Park, Bellandur",
+		lat: 12.9238,
+		lng: 77.6828,
+	},
+	{
+		name: "RGA Tech Park, Sarjapur Road",
+		lat: 12.9038,
+		lng: 77.6976,
+	},
+	{
+		name: "RMZ Millenia, Ulsoor",
+		lat: 12.9734,
+		lng: 77.6192,
+	},
+	{
+		name: "SJR I-Park, Whitefield",
+		lat: 12.984,
+		lng: 77.7345,
+	},
+	{
+		name: "Gold Hill Square, Hosur Road",
+		lat: 12.8953,
+		lng: 77.6366,
+	},
+	{
+		name: "Gopalan Signature Tower, Old Madras Road",
+		lat: 12.9922,
+		lng: 77.6612,
+	},
+	{
+		name: "DivyaSree Greens, Domlur (near EGL)",
+		lat: 12.953,
+		lng: 77.6415,
+	},
+	{
+		name: "Sigma Soft Tech Park, Whitefield",
+		lat: 12.9667,
+		lng: 77.7471,
+	},
+	{
+		name: "AMR Tech Park, Hosur Road",
+		lat: 12.905,
+		lng: 77.632,
+	},
+	{
+		name: "Helios Business Park, Kadubeesanahalli",
+		lat: 12.935,
+		lng: 77.694,
+	},
 	// Pune IBM Offices
 	{
 		name: "IBM Embassy Techzone (ETZ), Hinjewadi",
